5cd3ad5bc99e16ae832d9371dc781ebf12d32b7c
ozneroL541 <l.21radice@gmail.com>
2023-03-30 20:28:23 +0200

    zip file style made

--- src/climatemonitorig/ClimateMonitor.java
+++ src/climatemonitorig/ClimateMonitor.java
@@ -1,13 +1,13 @@
 /**************************************
- * Matricola    Nome        Cognome
- * 754530	    Galimberti	Riccardo
+ * Matricola    Cognome     Nome
+ * 754530       Galimberti	Riccardo
  * 753747       Masolo      Carlos
  * 755152       Paredi      Giacomo
  * 753252       Radice      Lorenzo
  * Sede: Como
 ***************************************/
 
-package climatemonitorig;
+package src.climatemonitorig;
 
 public class ClimateMonitor {
     public static void main(String[] args) {
