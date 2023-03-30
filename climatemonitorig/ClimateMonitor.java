0b2de6c4c096ed5018851e48924fdeb2074f4131
ozneroL541 <l.21radice@gmail.com>
2023-03-30 20:23:30 +0200

    header added

--- climatemonitorig/ClimateMonitor.java
+++ climatemonitorig/ClimateMonitor.java
@@ -1,3 +1,12 @@
+/**************************************
+ * Matricola    Nome        Cognome
+ * 754530	    Galimberti	Riccardo
+ * 753747       Masolo      Carlos
+ * 755152       Paredi      Giacomo
+ * 753252       Radice      Lorenzo
+ * Sede: Como
+***************************************/
+
 package climatemonitorig;
 
 public class ClimateMonitor {
