45031a86fc2ed30b57d22068e89a7c6d0c7ec195
ozneroL541 <l.21radice@gmail.com>
2023-04-03 20:01:46 +0200

    Headers added

--- src/monitoringcentre/MonitoringCentre.java
+++ src/monitoringcentre/MonitoringCentre.java
@@ -1,3 +1,12 @@
+/**************************************
+ * Matricola    Cognome     Nome
+ * 754530       Galimberti  Riccardo
+ * 753747       Masolo      Carlos
+ * 755152       Paredi      Giacomo
+ * 753252       Radice      Lorenzo
+ * Sede: Como
+***************************************/
+
 package src.monitoringcentre;
 
 public class MonitoringCentre {
