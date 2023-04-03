75f2688cb5a44c5fe11e6a6a4f21161f7e2bdbc3
ozneroL541 <l.21radice@gmail.com>
2023-04-03 18:45:01 +0200

    Header added

--- src/menu/MenuMaker.py
+++ src/menu/MenuMaker.py
@@ -1,4 +1,12 @@
 '''
+ * Matricola    Cognome     Nome
+ * 754530       Galimberti  Riccardo
+ * 753747       Masolo      Carlos
+ * 755152       Paredi      Giacomo
+ * 753252       Radice      Lorenzo
+ * Sede: Como
+'''
+'''
 This script is aimed at automacally generate a menu.
 It is not part of the program, it only creates a file in the specified format
 
