a0b8f2b82c703a91c0eae47912fde301b1a053e2
ozneroL541 <l.21radice@gmail.com>
2023-04-04 20:16:53 +0200

    Remove preexisting file

--- src/menu/MenuMaker.py
+++ src/menu/MenuMaker.py
@@ -14,9 +14,11 @@ It is not part of the program, it only creates a file in the specified format
 Always left the exit command a the end of the options array.
 Otherways the main Java Program will not work, the Menu class should be changed.
 '''
-#
+import os
+# Name of the file
 filename = "menu.txt"
 # Options array
+# Modify this array to modify the options
 options = [
         "Ricerca aree",
         "Visualizzazione parametri climatici associati",
@@ -31,16 +33,26 @@ separator = " - "
 # This function makes the string menu with numbers and with the separator
 # example:  "1 - Option1/n2 - option2" ...
 def Menu_Maker():
+    # Output string
     out = ""
+    # for ( int i = 0; i < lenght(options); i++ )
     for i in range(len(options)):
+        # Index string creation
         i_str = (str) (i + 1)
+        # Output string cration
         out += i_str +  separator + options[i] + '\n'
     return out
 # This function print the string on the fn file
 def PrintOnFile(fn, str_to_print ):
-    menu = Menu_Maker()
+    # Open the file
     file = open(fn, "wt")
-    n = file.write(str_to_print)
+    # Write the string ont the file
+    file.write(str_to_print)
+    # Close the file
     file.close()
+# If exists a file with the same name
+if os.path.isfile(filename):
+    # Delete the preexisting file
+    os.remove(filename)
 # Print the menu on filename
 PrintOnFile(filename, Menu_Maker() )
