b248ec2594b93d6ea6d98c823b60d0169f558064
ozneroL541 <l.21radice@gmail.com>
2023-04-08 16:40:58 +0200

    Check Codice Fiscale

--- src/users/AutorizedOperator.java
+++ src/users/AutorizedOperator.java
@@ -22,4 +22,65 @@ public class AutorizedOperator extends User {
     public AutorizedOperator() {
         //TODO
     }
+
+    // Check Codice Fiscale
+    private static boolean ControlloCodiceFiscale( String cf ) {
+        // Output declaration
+        boolean check = true;
+        // The length of Codice Fiscale must be 16 characters for fisical people
+        if ( cf.length() != 16 ) {
+            check = false;
+        } else {
+            // Index of the character which could not be letters
+            short [] int_index = { 6, 7, 9, 10, 12, 13, 14 } ;
+            // Index for the previos array
+            short j = 0;
+            // For every character of the array checkif the letters are in the correct position
+            for (short i = 0; check && i < cf.length(); i++) {
+                // If it could not be a letter increade the j index and go ahead
+                if ( j < int_index.length && i == int_index[j]) {
+                    j++;
+                } else {
+                    // If the current character is not a letter the string isn incorrect
+                    if ( ! Character.isLetter(cf.charAt(i)) ) {
+                        check = false;
+                    }
+                }
+            }
+            // If the string can still be true continue with the verification else terminate the execution fo the function
+            if (check) {
+                // Characters of the months
+                char[] m = { 'A', 'B', 'C', 'D', 'E', 'H', 'L', 'M', 'P', 'R', 'S', 'T' };
+                // This check if the month character is correct
+                boolean month_chek = false;
+                // Check for every character of the months
+                for (char c : m) {
+                    // If one of the months characters corrispond to the month charater in the string than true
+                    if ( c == Character.toUpperCase(cf.charAt(8)) ) {
+                        month_chek = true;
+                    }
+                }
+                // If the check of the month is correct continue
+                // If the first birthday character is a number continue else true
+                if ( check = month_chek && Character.isDigit(cf.charAt(9))) {
+                    // First  birthday character
+                    char c1 = cf.charAt(9);
+                    // Second birthday character 
+                    char c2 = cf.charAt(10);
+                    // If the first birthday digit is between 0 and 7 it is correct
+                    if ( ! ( ( c1 >= '0' && c1 < '3' ) || ( c1 >= '4' && c1 < '7' ) ) ) {
+                        // If the first birthday digit is 3 or 7 is ok
+                        if ( ! ( c1 == '3' || c1 == '7' ) ) {
+                            check = false;
+                        // Now the first digit must be 3 or 7, check if the second is a digit
+                        // If true than it must be 0 or 1 to be correct
+                        } else if ( Character.isDigit(c2) && ( ! ( c2 == '0' || c2 == '1' ) ) ) {
+                            check = false;
+                        }
+                    }
+                }
+            }
+        }
+        return check;
+    }
 }
