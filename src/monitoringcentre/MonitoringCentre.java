206fbbece1ce7291cd913707c3d20afd862ffee5
BiskoBerty <ricky.galimberti@gmail.com>
2023-06-22 16:34:17 +0200

    MonitoringCentre
    added constructor and one method to MonitoringCentre.java and created CentroMonitoraggio.dati.csv

--- src/monitoringcentre/MonitoringCentre.java
+++ src/monitoringcentre/MonitoringCentre.java
@@ -8,13 +8,63 @@
 ***************************************/
 
 package src.monitoringcentre;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Classe che contiene il centro di monitoraggio.
  * @author Lorenzo Radice
  * @version 0.0.0
  */
 public class MonitoringCentre {
+    // private String via, civico, cap, comune, provincia;
+    private String nome;
+    private String [] indirizzo = new String[5];
+    private String [] areeInteresse;
+    private short userid;
+
+    public MonitoringCentre(String nome, String [] indirizzo, String [] areeInteresse, short userid){
+        registraCentroAree(nome, indirizzo, areeInteresse, userid);
+    }
+
     public MonitoringCentre(){
-        //TODO
+        
+    }
+
+    public void registraCentroAree(String nome, String [] indirizzo, String [] areeInteresse, short userid){
+        this.nome = nome;
+        if(indirizzo.length == this.indirizzo.length){
+            this.indirizzo = indirizzo;
+        }else{
+            //TODO exception wrong array input length
+        }
+        if(areeInteresse.length > 0){
+            this.areeInteresse = areeInteresse;
+        }else{
+            //TODO exception empty list
+        }
+        this.userid = userid;
+        memorizzaCentroAree(nome, indirizzo, areeInteresse, userid);
+    }
+    
+    private void memorizzaCentroAree(String nome, String [] indirizzo, String [] areeInteresse, short userid){
+        String s = nome + ",";
+        for (int i = 0; i < indirizzo.length; i++) {
+            s = s + indirizzo[i] + ",";
+        }
+        for (int i = 0; i < areeInteresse.length; i++) {
+            s = s + areeInteresse[i] + ",";
+        }
+        s = s + userid;
+        try(FileWriter fw = new FileWriter("data\\CentroMonitoraggio.dati.csv", true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter out = new PrintWriter(bw)){
+            out.println(s);
+        } catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
     }
 }
