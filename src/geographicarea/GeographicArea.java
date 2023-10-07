7f969d70b5f7a489564606f5abe62ed3ab4fe2ac
ozneroL541 <l.21radice@gmail.com>
2023-10-07 13:04:20 +0200

    Versining

--- src/geographicarea/GeographicArea.java
+++ src/geographicarea/GeographicArea.java
@@ -20,7 +20,7 @@ import src.research.Research;
  * rappresenta un area geografica identificata con id,
  * nome, nome ASCII, stato e coordinate.
  * @author Lorenzo Radice
- * @version 0.2.1
+ * @version 0.2.2
  */
 public class GeographicArea {
     // Geoname ID
