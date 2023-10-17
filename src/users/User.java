ad04c5d7216b78aab63a198661311fbe560a18cd
ozneroL541 <l.21radice@gmail.com>
2023-10-17 14:52:52 +0200

    Main Test removed

--- src/users/User.java
+++ src/users/User.java
@@ -101,9 +101,4 @@ public class User {
         // Close Scanner
         sc.close();
     }
-    
-    // TODO remove test main
-    public static void main(String[] args) {
-        User.Ricerca();
-    }
 }
