ff287d3d0e88d89a4ddf84d3440cd408c1c91018
ozneroL541 <l.21radice@gmail.com>
2023-10-24 19:20:59 +0200

    static constants

--- src/table/Table.java
+++ src/table/Table.java
@@ -20,17 +20,24 @@ import java.util.Scanner;
  */
 public class Table {
     // Number of categories
-    private final short n_categories = 7;
+    private final static short n_categories = 7;
     // Max characters number for notes
-    private final short max_char_notes = 256;
+    private final static short max_char_notes = 256;
     // Min score
-    private final short min_score = 1;
+    private final static short min_score = 1;
     // Max score
-    private final short max_score = 5;
+    private final static short max_score = 5;
     // Category's scores
-    private short[] scores = { 1, 1, 1, 1, 1, 1, 1, };
+    private short[] scores = { 1, 1, 1, 1, 1, 1, 1 };
     // Category's notes
-    private String[] notes = { "", "", "", "", "", "", "", };
+    private String[] notes = { "", "", "", "", "", "", "" };
+    // Score and note
+    private class Data {
+        // Score
+        private short score = 0;
+        // Note
+        private String note = "";
+    }
     /**
      * Costruttore vuoto
      */
@@ -44,12 +51,12 @@ public class Table {
      */
     public Table( short[] s ) {
         // If the lenght of the array is not equals to the number of the categories lunch an exception
-        if ( s == null || s.length != this.n_categories ) {
+        if ( s == null || s.length != n_categories ) {
             // Error output because the lenght is not valid
             System.err.println("Errore: lunghezza array valori parametri tabella errata.");
         } else {
             // For every category assign the correct score
-            for ( short i = 0; i < this.n_categories; i++) {
+            for ( short i = 0; i < n_categories; i++) {
                 // If the score is valid assign it
                 if ( s[i] >= min_score  && s[i] <= max_score ) {
                     this.scores[i] = s[i];
@@ -72,7 +79,7 @@ public class Table {
      */
     public Table( short[] s, String[] note ) {
         // If the lenght of the arraies is not equals to the number of the categories lunch exceptions
-        if ( s == null || s.length != this.n_categories ) {
+        if ( s == null || s.length != n_categories ) {
             // Exception because the lenght is not valid
             System.err.println("Errore: lunghezza array valori parametri tabella errata.");
         } else if ( note == null || note.length != n_categories ) {
@@ -80,7 +87,7 @@ public class Table {
             System.err.println("Errore: numero valori note tabella errati.");
         } else {
             // For every category
-            for ( short i = 0; i < this.n_categories; i++) {
+            for ( short i = 0; i < n_categories; i++) {
                 // If the score is valid assign it
                 if ( s[i] >= min_score  && s[i] <= max_score ) {
                     this.scores[i] = s[i];
@@ -90,7 +97,7 @@ public class Table {
                         System.err.println("Lunghezza nota errata.\nLunghezza massima: 256 caratteri.");
                     } else {
                         // Assign the note
-                        this.notes = note;
+                        this.notes[i] = note[i];
                     }
                 } else {
                     // Exception, scores must be between 1 and 5
@@ -99,6 +106,35 @@ public class Table {
             }
         }
     }
+    /*
+     * Costruttore con classe privata Data
+     */
+    public Table( Data[] d ) {
+        if ( d == null && d.length != n_categories ) {
+            // Exception because the lenght is not valid
+            System.err.println("Errore: lunghezza array valori dati tabella errata.");
+        } else {
+            // For every category
+            for (int i = 0; i < n_categories; i++) {
+                // If the score is valid assign it
+                if ( d[i].score >= min_score  && d[i].score <= max_score ) {
+                    this.scores[i] = d[i].score;
+                    // If the string is less than 256 assign it
+                    if ( d[i].note.length() > max_char_notes ) {
+                        // Exception, note must be 256 character or less
+                        System.err.println("Lunghezza nota errata.\nLunghezza massima: 256 caratteri.");
+                    } else {
+                        // Assign the note
+                        this.notes[i] = d[i].note;
+                    }
+                } else {
+                    // Exception, scores must be between 1 and 5
+                    System.err.println("Errore: i valori fuori intervallo(1-5).");
+                }
+            }
+        }
+    }
+    /**
     /**
      * Restituisce la tabella sotto formato di stringa
      */
@@ -131,7 +167,7 @@ public class Table {
         // Start adding the header to the output string
         str += table_header;
         // For every category
-        for ( short i = 0; i < this.n_categories; i++ ) {
+        for ( short i = 0; i < n_categories; i++ ) {
             // Add the line of the table
             str += "\n" + climate_categories[i] + "\t" + explanations[i] + "\t  " + this.scores[i] + "\t" + this.notes[i];
         }
@@ -141,11 +177,18 @@ public class Table {
      * Chiede all'utante di inserire i parametri della ricerca e ritorna la tabella che li contiene
      */
     public static Table MakeTable() {
+        // Input scores
+        Integer[] scores = new Integer[n_categories];
+        // Input notes
+        String[] notes = new String[n_categories];
+        // Datas Input
+        Data[] datas = new Data[n_categories];
         // TODO
         try {
             // Input
             Scanner sc = new Scanner(System.in);
             //TODO
+
             
         } catch ( InputMismatchException e ) {
             // Error output
@@ -155,5 +198,11 @@ public class Table {
             e.printStackTrace();
         }
         return null;
+    }
+
+
+    public static void main(String[] args) {
+        Table t = new Table();
+        System.out.println(t.toString());
     }
 }
