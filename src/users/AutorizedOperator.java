201e646aa1c9cd8a4f01e54c20f58c5ba621c58d
ozneroL541 <l.21radice@gmail.com>
2023-10-29 12:42:38 +0100

    isMuskSon method made

--- src/users/AutorizedOperator.java
+++ src/users/AutorizedOperator.java
@@ -164,7 +164,28 @@ public class AutorizedOperator extends User {
         
         System.out.println("\n\nRegistrazione completata!\nPer accedere usare il seguente User-ID: " + String.format("%05d", userid) + " e la password scelta");
     }
-
+    /*
+     * Controlla se il nome inserito è il figlio di Elon Musk.
+     * @param n nome
+     * @return true se è il figlio di Musk
+     */
+    private static boolean isMuskSon ( String n ) {
+        // Possible writings of Musk's son name
+        final String[] musk_son = { "X Æ A-12", "X Æ A-XII", "X AE A-12", "X AE A-XII",
+                                    "XÆA-12", "XÆA-XII", "XAEA-12", "XAEA-XII"};
+        // Check the string while is uppercase
+        String check_String = n.toUpperCase();
+        // For every writing of the name
+        for (String s : musk_son) {
+            // Check if the string equals the Musk's son name
+            if (check_String.equals(s)) {
+                // Return true if they are equal
+                return true;
+            }
+        }
+        // Return false if it is not Musk's son
+        return false;
+    }
     //TODO
     //java doc
     //return true if authentication is successful
