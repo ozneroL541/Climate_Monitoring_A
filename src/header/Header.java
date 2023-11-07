40605aae9c00a63739c34dfdb6eda6b8353ec26d
ozneroL541 <l.21radice@gmail.com>
2023-11-07 18:55:40 +0100

    methods

--- src/header/Header.java
+++ src/header/Header.java
@@ -22,19 +22,62 @@
     along with Climate Monitoring.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package src.license;
+package src.header;
 //TODO
-public class License {
+public class Header {
     // TODO: change show or create options
-    // Program header
-    private final static String header =
+    // Program Name
+    private final static String p_name = "Climate Monitoring";
+    // Short License Reminder
+    private final static String license_h =
             "    Climate Monitoring  Copyright (C) 2024  Galimberti Riccardo, Paredi Giacomo, Radice Lorenzo\r\n" + //
             "    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\r\n" + //
             "    This is free software, and you are welcome to redistribute it\r\n" + //
             "    under certain conditions; type `show c' for details.";
-    // TODO 
-    public static void print() {
+    // Program version
+    private final static String version = "0.10.0";
+    /**
+     * Stampa a schermo un breve messaggio con la licenza.
+     */
+    public static void print_license() {
         // Print License
-        System.out.println(header);
+        System.out.println(license_h);
+    }
+    /**
+     * Stampa a schermo la versione del programma.
+     */
+    public static void print_version() {
+        // Print version
+        System.out.println(p_name + "\tVersion: " + version);
+    }
+    /**
+     * Stampa a schermo la versione e la licenza del programma.
+     */
+    public static void print_header() {
+        // Print License
+        System.out.print("    ");
+        print_version();
+        print_license();
+    }
+    /**
+     * Ritorna il nome del programma
+     * @return p_name
+     */
+    public static String getpName() {
+        return p_name;
+    }
+    /**
+     * Ritorna il messaggio contenete la licenza del software.
+     * @return license_h
+     */
+    public static String getLicenseH() {
+        return license_h;
+    }
+    /**
+     * Ritorna la versione del programma
+     * @return version
+     */
+    public static String getVersion() {
+        return version;
     }
 }
