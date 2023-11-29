d81aa6af5f02a5b952caf0626592191d9a9f6c3a
ozneroL541 <l.21radice@gmail.com>
2023-11-30 00:43:12 +0100

    Parameters constructor

--- src/parameters/Parameters.java
+++ src/parameters/Parameters.java
@@ -43,7 +43,6 @@ public class Parameters {
     private final static File file = FileSystems.getDefault().getPath("data", "ParametriClimatici.dati.csv").toFile();
     // Header
     private final static String header = "Geoname ID,Data,Centro,Vento,Umidità,Pressione,Temperatura,Precipitazioni,Altitudine dei ghiacciai,Massa dei ghiacciai,Note Vento,Note Umidità,Note Pressione,Note Temperatura,Note Precipitazioni,Note Altitudine dei ghiacciai,Note Massa dei ghiacciai";
-    // TODO Implement
     // Geoname ID
     private short geoname_id = 0;
     // Date
@@ -61,8 +60,105 @@ public class Parameters {
         private static final short table_length = Table.n_categories;
         private static final short max_cols = (table + table_length);
     }
-    // TODO Constructor
-    // TODO inserti ID, Data and Centre
+    /**
+     * Costruttore dell'oggetto Parameters
+     * @param id geoname_id
+     * @param d date
+     * @param c centre
+     * @param t table
+     */
+    public Parameters( short id, Date d, String c, Table t){
+        this.geoname_id = id;
+        this.date = d;
+        this.centre = c;
+        this.table = t;
+    }
+    /**
+     * Crea un oggetto Parameters e lo ritorna.
+     * @return oggetto Parameters
+     */
+    public static Parameters MakeParameters() {
+        // Output
+        Parameters p = null;
+        // Geoname ID
+        short id = 0;
+        // Date
+        Date d = null;
+        // Monitoring Centre
+        String c = "";
+        // Table
+        Table t = null;
+        // Assign Geoname ID
+        id = insertID();
+        // Check ID
+        if ( ! GeographicArea.IndexExist(id) ) {
+            // Error output
+            System.err.println("Errore nell'assegnazione dell'Area Geografica.");
+            // return Error
+            return null;
+        }
+        // Assign Date
+        d = insertDate();
+        // Check Date
+        if ( d == null ) {
+            // Error output
+            System.err.println("Errore nell'assegnazione della data.");
+            // return Error
+            return null;
+        }
+        // Assign Centre
+        c = insertCentre();
+        // Check Centre
+        if ( c == null || c.length() <= 0) {
+            // Error output
+            System.err.println("Errore nell'assegnazione del centro.");
+            // return Error
+            return null;
+        }
+        // Assign Table
+        t = Table.MakeTable();
+        // Check Table existance
+        if ( t == null ) {
+            // Error output
+            System.err.println("Errore nella creazione della tabella.");
+            // return Error
+            return null;
+        }
+        // MakeParameters
+        p = new Parameters(id, d, c, t);
+        // Return Parameters
+        return p;
+    }
+    /*
+     * Richiede l'inserimento del Geoname ID
+     * @return geoname_id
+     */
+    private static short insertID() {
+        // Geoname ID
+        short id = 0;
+        //TODO
+        return id;
+    }
+    /*
+     * Richiede o assegna la data.
+     * @return date
+     */
+    private static Date insertDate() {
+        // Date
+        Date date = null;
+        //TODO
+        return date;
+    }
+    /*
+     * Richiede l'inserimento del centro di monitoraggio
+     * @return centre
+     */
+    private static String insertCentre() {
+        // Centre
+        String centre = null;
+        // TODO
+        return centre;
+    }
     /*
      * Trasforma i campi della classe in un array di stringhe.
      * @return array di Strings
