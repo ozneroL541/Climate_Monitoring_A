870d7fde2ad1951dca756f245533988ce1d8dc63
ozneroL541 <l.21radice@gmail.com>
2023-12-07 12:31:54 +0100

    automaci.sh

--- automatic.sh
+++ automatic.sh
@@ -23,17 +23,25 @@
 
 # GNU Bash automatic compiler for ClimateMonitor program
 
-# Move in a directory different from src
+# Paths
+bin="bin/"
+doc="doc/"
+src="src/"
+lib="lib/"
+tmp="tmp/"
+# Executable
+jar="ClimateMonitor.jar"
+# Do not remove
+description=".description.txt"
 
-# Check the Path
-if  [ "$(basename "$(pwd)")" == "Climate_Monitoring" ]; then
-        # Create tmp directory
-    if mkdir tmp && cd tmp && mkdir lib && cd lib
+# Make Temporary File
+function maketmp {
+    if rm -r tmp; mkdir $tmp && cd $tmp && mkdir $lib && cd $lib
     then
         echo ""
         echo "tmp dir making: succeed"
         echo ""
-        if jar -xf ../../lib/opencsv-5.5.2.jar
+        if jar -xf ../../$lib"opencsv-5.5.2.jar"
         then
             echo ""
             echo "JAR extraction: succeed"
@@ -42,53 +50,19 @@ if  [ "$(basename "$(pwd)")" == "Climate_Monitoring" ]; then
             echo ""
             echo "JAR extraction: failed"
             echo ""
+            exit -1
         fi
         cd ../..
     else
         echo ""
         echo "tmp dir making: failed"
         echo ""
+        exit -1
     fi
-    # Compile java
-    if javac src/*/*.java -d bin/ -cp tmp/lib/
-    then
-        echo ""
-        echo "Compilation: succedeed"
-        echo ""
-        if cd bin
-        then
-            # Make an executable JAR
-            if jar cvfe ClimateMonitor.jar src.climatemonitoring.ClimateMonitor src/*/*.class
-            then
-                rm -r src
-                echo ""
-                echo "JAR creation: succeed"
-                echo ""
-            else
-                echo ""
-                echo "JAR creation: failed"
-                echo ""
-            fi
-            cd ..
-        fi
-    else
-        echo ""
-        echo "Compilation: failed"
-        echo ""
-    fi
-    # Make JavaDoc
-    if javadoc src/*/*.java -d doc/ -cp tmp/lib/
-    then
-        echo ""
-        echo "JavaDoc creation: succeed"
-        echo ""
-    else
-        echo ""
-        echo "JavaDoc creation: failed"
-        echo ""
-    fi
-    # Remove Temporary Directory
-    if rm -r tmp
+}
+# Remove Temporary
+function rmtmp {
+    if rm -r $tmp
     then
         echo ""
         echo "Execution: succedeed"
@@ -97,7 +71,107 @@ if  [ "$(basename "$(pwd)")" == "Climate_Monitoring" ]; then
         echo ""
         echo "Execution: failed"
         echo ""
+        exit -1
+    fi
+}
+# Compile
+function compile {
+    # Compile java
+    if javac $src*/*.java -d $bin -cp $tmp$lib
+    then
+        echo ""
+        echo "Compilation: succedeed"
+        echo ""
+        if cd $bin
+        then
+            # Make an executable JAR
+            if jar cvfe $jar src.climatemonitoring.ClimateMonitor $src*/*.class
+            then
+                rm -r $src
+                echo ""
+                echo "JAR creation: succeed"
+                echo ""
+            else
+                echo ""
+                echo "JAR creation: failed"
+                echo ""
+                exit -1
+            fi
+            cd ..
+        fi
+    else
+        echo ""
+        echo "Compilation: failed"
+        echo ""
+        exit -1
+    fi
+}
+# Remove JAR
+function rmjar {
+    if rm $bin$jar
+    then
+        echo ""
+        echo "JAR removing: succeed"
+        echo ""
+    else
+        echo ""
+        echo "JAR removing: failed"
+        echo ""
+        exit -1
+    fi
+}
+# Document
+function document {
+    if javadoc $src*/*.java -d $doc -cp $tmp$lib
+    then
+        echo ""
+        echo "Documentation creation: succeed"
+        echo ""
+    else
+        echo ""
+        echo "Documentation creation: failed"
+        echo ""
+        exit -1
+    fi
+}
+# Remove Documetation
+function rmdoc {
+    if cd $doc
+    then
+        # Delete all files and directories except description
+        if find . ! -name $description -type f -delete && find . -mindepth 1 -maxdepth 1 ! -name $description -type d -exec rm -rf {} +
+        then
+            echo ""
+            echo "Documentation removing: succeed"
+            echo ""
+        else
+            echo ""
+            echo "Documentation removing: failed"
+            echo ""
+            exit -1
+        fi
+
+        cd ..
+    fi
+}
+
+
+# Move in a directory different from src
+
+# Check the Path
+if  [ "$(basename "$(pwd)")" == "Climate_Monitoring" ]; then
+    # Create tmp directory
+    if maketmp
+    then
+        # Compile Java and Make JAR
+        compile
+        # Make JavaDoc
+        document
+        # Remove Temporary Directory
+        rmtmp
     fi
 else
     echo "Error: Wrong Path"
 fi
+
+
