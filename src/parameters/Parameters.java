a5a5e267fbffdbc8706d95c1463d1c5b99cbb6ac
ozneroL541 <l.21radice@gmail.com>
2023-12-08 18:34:58 +0100

    output

--- src/parameters/Parameters.java
+++ src/parameters/Parameters.java
@@ -242,7 +242,7 @@ public class Parameters {
         }
 
         //user choose area
-        System.out.print("\nScegliere l'area inserendone il codice: ");
+        System.out.print("\nScegliere l'area inserendone il Geoname ID: ");
         do{
             area=InputScanner.INPUT_SCANNER.nextLine();
             //check if user input is a valid id
