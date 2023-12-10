102ab5254547cbba540b026db8e9f5199b55f695
ozneroL541 <l.21radice@gmail.com>
2023-12-11 00:27:16 +0100

    Remove test main

--- automatic.sh
+++ automatic.sh
@@ -101,7 +101,7 @@ function compile_jar {
 # Compile to Objects
 function compile {
     # Compile java
-    javac $src*/*.java -d $bin -cp $lib"opencsv-5.5.2.jar" $lib"commons-lang3-3.1.jar"
+    javac -cp $lib"opencsv-5.5.2.jar:"$lib"commons-lang3-3.1.jar:." $src*/*.java -d $bin
     result $? "Compilation"
 }
 # Remove Objects files
