0fae9d52fa46f942e44d72a2d67c4fef2b2b753a
ozneroL541 <l.21radice@gmail.com>
2024-01-19 19:31:49 +0100

    Doc

--- src/common/CSV_Utilities.java
+++ src/common/CSV_Utilities.java
@@ -49,7 +49,7 @@ public class CSV_Utilities {
      * Se il file CSV è vuoto o non ha linee aggiunge l'intestazione.
      * Ritorna true se l'esecuzione è avvenuta correttamente.
      * <br><br>Complessità
-     * <br>T = θ(n)
+     * <br>T = O(n)
      * @param file file CSV
      * @param linecells celle della linea da aggiungere
      * @param header intestazione
@@ -291,7 +291,7 @@ public class CSV_Utilities {
      * Si può scegliere se aggiungere una nuova linea alla fine della scrittura.
      * Ritorna true se la scrittura ha avuto esito positivo, false se non è avvenuta.
      * <br><br>Complessità
-     * <br>T = θ(n)
+     * <br>T = O(1)
      * @param file file CSV
      * @param line linea
      * @param add_newline aggiunge una nuova linea se true
